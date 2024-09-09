3128a44240e411dd129ea5ed1810723a807b42c3
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2024-09-09 14:26:08 +0700

    Updated docstrings to comply with flake8

--- statistics.py
+++ statistics.py
@@ -1,3 +1,4 @@
+"""Module for common math variables"""
 from math import sqrt
 
 
@@ -9,7 +10,7 @@ def average(data):
 
 
 def variance(data):
-    """the population variance of a list of numbers in data.
+    """The population variance of a list of numbers in data.
 
     The variance is the sum of squared differences between data values
     and their mean, divided by the number of items in the list.
@@ -17,7 +18,6 @@ def variance(data):
     which returns the sample variance, where the sum is divided by (n-1).
 
     Example: variance([1,5]) is ((1-3)**2 + (5-3)**2)/2 = 4.
-
     :param data: list of numbers for which variance will be computed. 
            Must contain at least one element.
     :returns: population variance of values in data list.
@@ -39,5 +39,5 @@ def variance(data):
 
 
 def stdev(data):
-    """the standard deviation of a list of values"""
+    """The standard deviation of a list of values."""
     return sqrt(variance(data))
